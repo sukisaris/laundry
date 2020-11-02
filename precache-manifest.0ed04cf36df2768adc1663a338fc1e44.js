fb99181ee2dccc800a3586add69c8f9f2aef802f
Sukis Aris <arissukiswanto2@gmail.com>
2020-11-02 18:54:35 +0700

    Updates

---
+++ precache-manifest.0ed04cf36df2768adc1663a338fc1e44.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "188e39f8b7610bfb1ab6050f83f127d6",
+    "url": "/laundry/index.html"
+  },
+  {
+    "revision": "089956a8c65688ba62a2",
+    "url": "/laundry/static/css/main.cb8d209c.chunk.css"
+  },
+  {
+    "revision": "eca1458021cebecaa2e5",
+    "url": "/laundry/static/js/2.b907b6fd.chunk.js"
+  },
+  {
+    "revision": "736f3ad4ee5990319c2a87119f7ebea6",
+    "url": "/laundry/static/js/2.b907b6fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "089956a8c65688ba62a2",
+    "url": "/laundry/static/js/main.1a19008a.chunk.js"
+  },
+  {
+    "revision": "972b8f8d4ea5e80f8556",
+    "url": "/laundry/static/js/runtime-main.643b71b9.js"
+  },
+  {
+    "revision": "ca6f7092af0bfcaca3aa6b176a790a7d",
+    "url": "/laundry/static/media/account.ca6f7092.svg"
+  },
+  {
+    "revision": "0e54ade9a8cf0f740bc15aad761f715e",
+    "url": "/laundry/static/media/add-user.0e54ade9.svg"
+  },
+  {
+    "revision": "b0eb8ad7861ff0d572a14f377015de63",
+    "url": "/laundry/static/media/dashboard.b0eb8ad7.svg"
+  },
+  {
+    "revision": "520185f9a7cb759929efb256503e574b",
+    "url": "/laundry/static/media/shirt.520185f9.svg"
+  },
+  {
+    "revision": "095f317cc641836fbf62885fc9e9affd",
+    "url": "/laundry/static/media/shirt2.095f317c.svg"
+  },
+  {
+    "revision": "73e4690a079a71c3718a85fee1ecaa78",
+    "url": "/laundry/static/media/shirts.73e4690a.svg"
+  },
+  {
+    "revision": "5e0e6de6e1989286b3f600453821050b",
+    "url": "/laundry/static/media/transaction.5e0e6de6.svg"
+  },
+  {
+    "revision": "b1c2b6a0d3766ba18378ecc3717a9822",
+    "url": "/laundry/static/media/user.b1c2b6a0.svg"
+  },
+  {
+    "revision": "193e8faec7243c4da9e52f8cfd9c1c43",
+    "url": "/laundry/static/media/wallet.193e8fae.svg"
+  }
+]);
